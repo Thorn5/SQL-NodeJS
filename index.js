fac3ff2fb8883f7ae9cf954af3fdb4e35076cbe7
Trygve Horn <trygvehorn@gmail.com>
2023-03-29 14:33:13 +0200

    Commit_2

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
+// npm init
 // npm install express --save
-// npm install nodemon
-// npm install dotenv --save
+// npm install nodemon --save
+// npm install pg --save // postgres pool
+// npm install dotenv --save // access .env
+// npm install body-parser --save // needed for req.body
+// .env PGPORT=5432 (postgres default port)
+// package.json entry "start": "nodemon index.js",
 
-require('dotenv').config()
 const express = require('express');
 const app = express();
+const router = express.Router();
+require('dotenv').config()
+// console.log(process.env) // working
+const port = 8001
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+const { Pool } = require('pg');
+const { query } = require('express');
+// const { query } = require('express');
+const pool = new Pool();
 
-console.log(process.env) // remove this after you've confirmed it is working
+//USERS
+app.get("/users/", (req, res) => {
+  pool
+  .query('SELECT * from users;')
+  .then(({rows}) => res.json(rows))
+  // .catch(e => res.sendStatus(404))
+  .catch(e => {
+    console.log(e)
+    res.sendStatus(500)
+});
+})
+
+app.get("/api/users/:id", (req, res) => {
+  const {id} = req.params;
+  console.log(id);
+  pool
+  .query('SELECT * from users WHERE id=$1;', [id])
+  .then(({rows}) => res.json(rows))
+  // .catch(e => res.sendStatus(404))
+  .catch(e => {
+    console.log(e)
+    res.sendStatus(500)
+});
+})
+
+//ORDERS
+app.get("/orders/", (req, res) => {
+  pool
+  .query('SELECT * from orders;')
+  .then(({rows}) => res.json(rows))
+  // .catch(e => res.sendStatus(404))
+  .catch(e => {
+    console.log(e)
+    res.sendStatus(500)
+});
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
